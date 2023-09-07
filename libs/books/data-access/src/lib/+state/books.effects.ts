ec63257b0905371617ee4f3b744834250dfc8926
Vamsi Krishna <vamsikrishna.saragadam1@t-mobile.com>
2023-09-08 01:52:05 +0530

    added instant search feature and handled test cases

--- libs/books/data-access/src/lib/+state/books.effects.ts
+++ libs/books/data-access/src/lib/+state/books.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { catchError, debounceTime, map, switchMap } from 'rxjs/operators';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Book } from '@tmo/shared/models';
 import * as BooksActions from './books.actions';
@@ -11,6 +11,7 @@ export class BooksEffects {
   searchBooks$ = createEffect(() =>
     this.actions$.pipe(
       ofType(BooksActions.searchBooks),
+      debounceTime(500),
       switchMap((action) =>
         this.http.get<Book[]>(`/api/books/search?q=${action.term}`).pipe(
           map((data) => BooksActions.searchBooksSuccess({ books: data })),
